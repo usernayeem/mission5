d7ee13a21472fa615757c7d12a4bb8ec1d863d26
usernayeem <93907431+usernayeem@users.noreply.github.com.>
2026-02-21 16:45:36 +0600

    feat: add Patient model and update registration process to create patient profile

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { UserStatus } from "../../../generated/prisma/enums";
 import { auth } from "../../lib/auth";
+import { prisma } from "../../lib/prisma";
 
 interface IRegisterPatientPayload {
   name: string;
@@ -22,7 +23,28 @@ const registerPatient = async (payload: IRegisterPatientPayload) => {
     throw new Error("Failed to register patient");
   }
 
-  return register;
+  // Create Patient Profile In Transaction After Sign Up Of Patient In USer Model
+  try {
+    const patient = await prisma.$transaction(async (tx) => {
+      const patientTx = await tx.patient.create({
+        data: {
+          userId: register.user.id,
+          name: payload.name,
+          email: payload.email,
+        },
+      });
+      return patientTx;
+    });
+
+    return { ...register, patient };
+  } catch (error) {
+    await prisma.user.delete({
+      where: {
+        id: register.user.id,
+      },
+    });
+    throw error;
+  }
 };
 
 interface ILoginUserPayload {
